885b79e06466499c832594b407c26cff6cb5ad38
Cleiton <cleitonwoycik@outlook.com>
2022-04-01 22:47:46 -0300

    feat: ensure ArticleMongoRepository load all articles

--- src/infra/repositories/article-mongo-repository.ts
+++ src/infra/repositories/article-mongo-repository.ts
@@ -1,9 +1,20 @@
-import { SaveArticlesRepository } from '@/data/protocols'
+import { SaveArticlesRepository, LoadArticlesRepository } from '@/data/protocols'
 import { MongoHelper } from './mongo-helper'
 
-export class ArticleMongoRepository implements SaveArticlesRepository {
+export class ArticleMongoRepository implements SaveArticlesRepository, LoadArticlesRepository {
   async save (articles: SaveArticlesRepository.Params): Promise<void> {
     const articleCollection = await MongoHelper.getCollection('articles')
     await articleCollection.insertMany(articles)
+  }
+
+  async loadAll (data: LoadArticlesRepository.Params): Promise<LoadArticlesRepository.Result> {
+    const { offset, limit } = data
+    const articleCollection = await MongoHelper.getCollection('articles')
+    const articles = await articleCollection.find()
+      .sort({ publishedAt: -1 })
+      .skip(offset)
+      .limit(limit)
+      .toArray()
+    return MongoHelper.mapCollection(articles)
   }
 }
