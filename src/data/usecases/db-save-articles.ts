62d6da03701e6ae5d262ab1b72910179111479e7
Cleiton <cleitonwoycik@outlook.com>
2022-04-02 16:30:27 -0300

    feat: ensure DbSaveArticles calls GetArticlesApi with crorrect param

--- src/data/usecases/db-save-articles.ts
+++ src/data/usecases/db-save-articles.ts
@@ -1,12 +1,18 @@
 import { SaveArticlesJob } from '@/domain/contracts'
-import { CountArticlesRepository } from '@/data/protocols'
+import { CountArticlesRepository, GetAllArticlesApi } from '@/data/protocols'
 
 export class DbSaveArticles implements SaveArticlesJob {
   constructor (
-    private readonly countRepository: CountArticlesRepository
+    private readonly countRepository: CountArticlesRepository,
+    private readonly getAllArticlesApi: GetAllArticlesApi
   ) {}
 
   async save (defaultUrl: string, url: string): Promise<void> {
-    await this.countRepository.count()
+    const count = await this.countRepository.count()
+    if (count === 0) {
+      await this.getAllArticlesApi.getAll(url)
+    } else {
+      await this.getAllArticlesApi.getAll(defaultUrl)
+    }
   }
 }
