27895f49b1910188de2d3ed2bf6c6711a2d0739d
Cleiton <cleitonwoycik@outlook.com>
2022-04-02 17:06:53 -0300

    test: ensure DbSaveArticles throws if any use cases throws

--- tests/data/db-save-articles.spec.ts
+++ tests/data/db-save-articles.spec.ts
@@ -42,6 +42,13 @@ describe('DbSaveArticles UseCase', () => {
     expect(countSpy).toHaveBeenCalled()
   })
 
+  it('Should throw if ContArticlesRepository throws', async () => {
+    const { sut, countRepository } = makeSut()
+    jest.spyOn(countRepository, 'count').mockRejectedValueOnce(new Error())
+    const promise = sut.save('any_default_url', 'any_url')
+    expect(promise).rejects.toThrow()
+  })
+
   it('Should call GetAllArticlesApi whit correct param', async () => {
     const { sut, countRepository, getAllArticlesApiSpy, loadDateOfLastArticleRepositorySpy } = makeSut()
     countRepository.resutl = 0
@@ -51,6 +58,13 @@ describe('DbSaveArticles UseCase', () => {
     await sut.save('any_default_url', 'any_url')
     const date = loadDateOfLastArticleRepositorySpy.result
     expect(getAllArticlesApiSpy.url).toBe('any_default_url' + date)
+  })
+
+  it('Should throw if GetAllArticlesApi throws', async () => {
+    const { sut, getAllArticlesApiSpy } = makeSut()
+    jest.spyOn(getAllArticlesApiSpy, 'getAll').mockRejectedValueOnce(new Error())
+    const promise = sut.save('any_default_url', 'any_url')
+    expect(promise).rejects.toThrow()
   })
 
   it('Should call SaveArticlesRepository whit correct params', async () => {
@@ -63,10 +77,24 @@ describe('DbSaveArticles UseCase', () => {
     expect(saveArticlesRepositorySpy.params).toEqual(getAllArticlesApiSpy.result)
   })
 
+  it('Should throw if SaveArticlesRepository throws', async () => {
+    const { sut, saveArticlesRepositorySpy } = makeSut()
+    jest.spyOn(saveArticlesRepositorySpy, 'save').mockRejectedValueOnce(new Error())
+    const promise = sut.save('any_default_url', 'any_url')
+    expect(promise).rejects.toThrow()
+  })
+
   it('Should call LoadDateOfLastArticleRepository', async () => {
     const { sut, loadDateOfLastArticleRepositorySpy } = makeSut()
     const loadDateSpy = jest.spyOn(loadDateOfLastArticleRepositorySpy, 'loadDate')
     await sut.save('any_default_url', 'any_url')
     expect(loadDateSpy).toHaveBeenCalled()
+  })
+
+  it('Should throw if LoadDateOfLastArticleRepository throws', async () => {
+    const { sut, loadDateOfLastArticleRepositorySpy } = makeSut()
+    jest.spyOn(loadDateOfLastArticleRepositorySpy, 'loadDate').mockRejectedValueOnce(new Error())
+    const promise = sut.save('any_default_url', 'any_url')
+    expect(promise).rejects.toThrow()
   })
 })
