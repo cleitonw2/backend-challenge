75434618b9ec52b55a768fb1f30cc5ad529329aa
Cleiton <cleitonwoycik@outlook.com>
2022-04-02 20:10:00 -0300

    test: ensure SaveArticlesJob returns null if SaveArticle throws

--- tests/presentation/jobs/SaveArticlesJob.spec.ts
+++ tests/presentation/jobs/SaveArticlesJob.spec.ts
@@ -22,4 +22,11 @@ describe('SaveArticles Job', () => {
     expect(saveArticlesSpy.defaultUrl).toBe('any_default_url')
     expect(saveArticlesSpy.url).toBe('any_url')
   })
+
+  it('Should return null if SaveArticle throws', async () => {
+    const { sut, saveArticlesSpy } = makeSut()
+    jest.spyOn(saveArticlesSpy, 'save').mockRejectedValueOnce(new Error())
+    const result = await sut.execute('any_default_url', 'any_url')
+    expect(result).toBe(null)
+  })
 })
