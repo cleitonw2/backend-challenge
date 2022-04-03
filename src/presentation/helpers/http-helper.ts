89a9e313b6224581c3e7d923259a000254306783
Cleiton <cleitonwoycik@outlook.com>
2022-04-03 09:35:13 -0300

    feat: ensure SaveArticleController returns  error if Validation return error

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,8 @@
+import { HttpResponse } from '@/presentation/protocols'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: {
+    error: error.message
+  }
+})
