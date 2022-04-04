816d6fbb3e609f9fe5f4beca091b82d985fa5c97
Cleiton <cleitonwoycik@outlook.com>
2022-04-04 10:13:03 -0300

    feat: ensure DeleteArticleController calls DeleteArticle with correct param

--- src/presentation/controllers/delete-article-controller.ts
+++ src/presentation/controllers/delete-article-controller.ts
@@ -1,15 +1,18 @@
+import { DeleteArticle } from '@/domain/contracts'
 import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class DeleteArticleController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly deleteArticle: DeleteArticle
   ) {}
 
   async handle (request: DeleteArticleController.Params): Promise<HttpResponse> {
     const { id } = request
     const error = this.validation.validate({ id: +id })
     if (error) return badRequest(error)
+    await this.deleteArticle.delete(+id)
     return null as any
   }
 }
