3681bd3e6b07f9c80b63d67ac452524ff54d71b4
Cleiton <cleitonwoycik@outlook.com>
2022-04-03 22:43:11 -0300

    feat: ensure UpdateArticle calls UpdateArticle with correct params

--- src/presentation/controllers/update-article-controller.ts
+++ src/presentation/controllers/update-article-controller.ts
@@ -1,15 +1,19 @@
+import { UpdateArticle } from '@/domain/contracts'
 import { Article } from '@/domain/models'
 import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class UpdateArticleController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly updateArticle: UpdateArticle
   ) {}
 
   async handle (request: UpdateArticleController.Params): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) return badRequest(error)
+    const { id, ...rest } = request
+    await this.updateArticle.update({ id: +id, ...rest })
     return null as any
   }
 }
