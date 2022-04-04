ac0bdac5e859d5cb85d81b3e222352a75c209136
Cleiton <cleitonwoycik@outlook.com>
2022-04-04 15:35:11 -0300

    refactor: move mongo url connection to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config'
+
 export const env = {
   port: process.env.PORT || 3000,
   defaultUrl: 'https://api.spaceflightnewsapi.net/v3/articles?publishedAt_gt=',
   url: 'https://api.spaceflightnewsapi.net/v3/articles?_limit=-1',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:docker@localhost:27017/api?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+  mongoUrl: process.env.MONGO_URL
 }
