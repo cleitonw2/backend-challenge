49570104d45bb5712bf482e51bedd3a6ea9538f5
Cleiton <cleitonwoycik@outlook.com>
2022-04-03 21:44:59 -0300

    feat: ensure LoadArticlesController returns 400 if Validation throws

--- src/presentation/controllers/load-article-by-id-controller.ts
+++ src/presentation/controllers/load-article-by-id-controller.ts
@@ -1,10 +1,12 @@
+import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class LoadArticleByIdController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (request: LoadArticleByIdController.Params): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) return badRequest(error)
     return null as any
   }
 }
