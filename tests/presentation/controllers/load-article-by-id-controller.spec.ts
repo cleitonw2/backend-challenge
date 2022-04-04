0e0a8d95bdb54d2c789a81fbdb409a0cf3686928
Cleiton <cleitonwoycik@outlook.com>
2022-04-03 21:45:15 -0300

    test: ensure LoadArticlesController returns 400 if Validation throws

--- tests/presentation/controllers/load-article-by-id-controller.spec.ts
+++ tests/presentation/controllers/load-article-by-id-controller.spec.ts
@@ -1,4 +1,5 @@
 import { LoadArticleByIdController } from '@/presentation/controllers'
+import { badRequest } from '@/presentation/helpers'
 import { ValidationSpy } from '../mocks'
 
 type SutTypes = {
@@ -22,7 +23,12 @@ describe('LoadArticleById Controller', () => {
     expect(validationSpy.input).toEqual({ id: '12345' })
   })
 
-  it.todo('Should return 400 if Validation return an error')
+  it('Should return 400 if Validation return an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.result = new Error()
+    const httpResponse = await sut.handle({ id: '12345' })
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
 
   it.todo('Should call LoadArticleById with correct param')
 
