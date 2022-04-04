39ab8de271ad4cd53e0e2d3ff4db2e3e126a36ed
Cleiton <cleitonwoycik@outlook.com>
2022-04-04 15:24:03 -0300

    refactor: move mongodb connection to server.ts file

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,12 +1,8 @@
-import { MongoHelper } from '@/infra/repositories'
 import express from 'express'
-import { env } from './config/env'
 import { routes } from './routes'
 
 const app = express()
 app.use(express.json())
-
-MongoHelper.connect(env.mongoUrl).catch(console.error)
 
 app.use(routes)
 
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,17 @@
 import 'module-alias/register'
+import { MongoHelper } from '@/infra/repositories'
 import cron from 'node-cron'
 import { app } from './app'
 import { env } from './config/env'
 import { makeSaveArticlesJob } from './factories/jobs'
 
-app.listen(env.port, () => {
-  console.log('server is runnig!')
-  cron.schedule('0 0 9 * * *', async () => {
+MongoHelper.connect(env.mongoUrl).then(() => {
+  app.listen(env.port, async () => {
+    console.log('server is runnig!')
     await makeSaveArticlesJob().execute(env.defaultUrl, env.url)
+
+    cron.schedule('0 0 9 * * *', async () => {
+      await makeSaveArticlesJob().execute(env.defaultUrl, env.url)
+    })
   })
-})
+}).catch(console.error)
