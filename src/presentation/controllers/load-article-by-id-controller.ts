cec0b4b128321a5fdd95c3da9b293f42e4344119
Cleiton <cleitonwoycik@outlook.com>
2022-04-03 21:50:15 -0300

    feat: ensure LoadArticleByIdController calls LoadArticleById with correct param

--- src/presentation/controllers/load-article-by-id-controller.ts
+++ src/presentation/controllers/load-article-by-id-controller.ts
@@ -1,12 +1,17 @@
+import { LoadArticleById } from '@/domain/contracts'
 import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class LoadArticleByIdController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly loadArticleById: LoadArticleById
+  ) {}
 
   async handle (request: LoadArticleByIdController.Params): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) return badRequest(error)
+    await this.loadArticleById.load(+request.id)
     return null as any
   }
 }
