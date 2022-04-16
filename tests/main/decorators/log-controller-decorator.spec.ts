2cdfac35c2965029b3b2eb168ff14c16a80f757b
Cleiton <cleitonwoycik@outlook.com>
2022-04-15 21:26:07 -0300

    test: ensure LogControllerDecorator calls LogErrorRepo with correct error

--- tests/main/decorators/log-controller-decorator.spec.ts
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -1,17 +1,22 @@
 import { LogControllerDecorator } from '@/main/decorators'
+import { serverError } from '@/presentation/helpers'
+import { LogErrorRepositorySpy } from '@/tests/data/mock-log'
 import { ControllerSpy } from '@/tests/presentation/mocks'
 
 type SutTypes = {
   sut: LogControllerDecorator
   controllerSpy: ControllerSpy
+  logErrorRepositorySpy: LogErrorRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const controllerSpy = new ControllerSpy()
-  const sut = new LogControllerDecorator(controllerSpy)
+  const logErrorRepositorySpy = new LogErrorRepositorySpy()
+  const sut = new LogControllerDecorator(controllerSpy, logErrorRepositorySpy)
   return {
     sut,
-    controllerSpy
+    controllerSpy,
+    logErrorRepositorySpy
   }
 }
 
@@ -23,5 +28,14 @@ describe('LogController Decorator', () => {
     }
     await sut.handle(request)
     expect(controllerSpy.request).toEqual(request)
+  })
+
+  it('Should call LogErrorRepo with correct error if Controller returns a server error', async () => {
+    const { sut, controllerSpy, logErrorRepositorySpy } = makeSut()
+    const error = new Error('fatal_error')
+    controllerSpy.httpResponse = serverError(error)
+    const httpResponse = await sut.handle('')
+    expect(logErrorRepositorySpy.stack).toEqual(error.stack)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
